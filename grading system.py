b8a0b75189cc30e74ad89f898b0613d51a58f8ae
Bharosa-kaushal <bharosakaushal1964@gmail.com>
2025-09-19 10:26:05 +0530

    Add files via upload

---
+++ grading system.py
@@ -0,0 +1,10 @@
+def calculater(a,b,operator):
+ if operator =="+":
+   print(a+b)
+ elif operator <=60:
+      print("c")
+ elif operator <=75:
+      print("b")
+ else:
+    print("a")
+calculater(20,20,"+")
